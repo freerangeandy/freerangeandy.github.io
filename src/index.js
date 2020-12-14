9c5ae2cd76da8dfc7ef765c18ef92ef6281e2d80
freerangeandy <freerangeandy@gmail.com>
2020-12-14 15:05:52 -0500

    add boilerplate index html/js files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { Component, Fragment } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h2>Hi</h2>
+      </Fragment>
+    )
+  }
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
