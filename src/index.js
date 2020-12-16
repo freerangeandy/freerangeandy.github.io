1859a9c32141a2cd52d79a0ad9f690ad1549c8f5
freerangeandy <freerangeandy@gmail.com>
2020-12-16 17:36:53 -0500

    add CV link & contact section header

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom'
 
 class App extends Component {
   render() {
+    const pdfFile = require('../assets/cv.pdf')
+
     return (
       <Fragment>
         <section id="about">
           <h1>Andy Lee</h1>
           <p>I'm a recent graduate of the Launch Academy boot camp with 7 years of prior software development experience - and a few years as a high school teacher sandwiched in-between. My favorite coding hobbies are building single-page web apps in JavaScript, trying out new 3rd-party APIs or libraries, and taking online courses in a new language.</p>
+          <a href={pdfFile} target="blank">CV</a>
         </section>
         <address id="contact">
+          <h3>Contact</h3>
           <a href="mailto:andy_lee@alum.mit.edu">Email</a><br />
           <a href="https://www.linkedin.com/in/andrew-lee-1701d/">LinkedIn</a>
         </address>
+        <hr/>
         <section id="projects">
           <h3>Projects</h3>
           <article>
