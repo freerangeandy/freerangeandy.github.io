6e4243c9f7f4bdcb4e68c4b4df5ecf9d1aee11ef
freerangeandy <freerangeandy@gmail.com>
2020-12-16 16:36:13 -0500

    add contact info & wrap name/blurb in section tag

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Andy Lee</h1>
-        <p>I'm a recent graduate of the Launch Academy boot camp with 7 years of prior software development experience - and a few years as a high school teacher sandwiched in-between. My favorite coding hobbies are building single-page web apps in JavaScript, trying out new 3rd-party APIs or libraries, and taking online courses in a new language.</p>
+        <section>
+          <h1>Andy Lee</h1>
+          <p>I'm a recent graduate of the Launch Academy boot camp with 7 years of prior software development experience - and a few years as a high school teacher sandwiched in-between. My favorite coding hobbies are building single-page web apps in JavaScript, trying out new 3rd-party APIs or libraries, and taking online courses in a new language.</p>
+        </section>
+        <address>
+          <a href="mailto:andy_lee@alum.mit.edu">Email</a><br />
+          <a href="https://www.linkedin.com/in/andrew-lee-1701d/">LinkedIn</a>
+        </address>
       </Fragment>
     )
   }
