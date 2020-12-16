bee531009a39eca16f251a3e689be789303a44c9
freerangeandy <freerangeandy@gmail.com>
2020-12-16 16:27:09 -0500

    add blurb about me

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ class App extends Component {
     return (
       <Fragment>
         <h1>Andy Lee</h1>
+        <p>I'm a recent graduate of the Launch Academy boot camp with 7 years of prior software development experience - and a few years as a high school teacher sandwiched in-between. My favorite coding hobbies are building single-page web apps in JavaScript, trying out new 3rd-party APIs or libraries, and taking online courses in a new language.</p>
       </Fragment>
     )
   }
