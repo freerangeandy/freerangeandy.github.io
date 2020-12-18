49ffb8d250a77b0eb094ab59f7cc3cf9e5a6fe4c
freerangeandy <freerangeandy@gmail.com>
2020-12-18 16:32:39 -0500

    added useState hook to fix menubar to top of window when scrolling down

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
+  Visibility,
   Segment,
   Container,
   Menu,
@@ -7,28 +8,40 @@ import {
 } from 'semantic-ui-react'
 
 const HeaderSection = (props) => {
+  const [fixed, setFixed] = useState(false)
   const pdfFile = require('../../assets/cv.pdf')
 
   return (
-    <Segment vertical inverted color="violet">
-      <Menu inverted color="violet" size="large">
-        <Container>
-          <Menu.Item as="a">About Me</Menu.Item>
-          <Menu.Item as="a">Experience</Menu.Item>
-          <Menu.Item as="a">Projects</Menu.Item>
-          <Menu.Item position="right">
-            <Button as="a">
-              Resumé
-            </Button>
-          </Menu.Item>
+    <Visibility
+      once={false}
+      onBottomPassed={() => setFixed(true)}
+      onBottomPassedReverse={() => setFixed(false)}
+    >
+      <Segment vertical inverted color="violet">
+        <Menu
+          fixed={fixed ? "top" : null}
+          inverted={!fixed}
+          color="violet"
+          size="large"
+        >
+          <Container>
+            <Menu.Item as="a">About Me</Menu.Item>
+            <Menu.Item as="a">Experience</Menu.Item>
+            <Menu.Item as="a">Projects</Menu.Item>
+            <Menu.Item position="right">
+              <Button as="a">
+                Resumé
+              </Button>
+            </Menu.Item>
+          </Container>
+        </Menu>
+        <Container text>
+          <h1>Andy Lee</h1>
+          <p>I'm a recent graduate of the Launch Academy boot camp with 7 years of prior software development experience - and a few years as a high school teacher sandwiched in-between. My favorite coding hobbies are building single-page web apps in JavaScript, trying out new 3rd-party APIs or libraries, and taking online courses in a new language.</p>
+          <a href={pdfFile} target="blank">CV</a>
         </Container>
-      </Menu>
-      <Container text>
-        <h1>Andy Lee</h1>
-        <p>I'm a recent graduate of the Launch Academy boot camp with 7 years of prior software development experience - and a few years as a high school teacher sandwiched in-between. My favorite coding hobbies are building single-page web apps in JavaScript, trying out new 3rd-party APIs or libraries, and taking online courses in a new language.</p>
-        <a href={pdfFile} target="blank">CV</a>
-      </Container>
-    </Segment>
+      </Segment>
+    </Visibility>
   )
 }
 
