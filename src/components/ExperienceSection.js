5af387faabf9b650eb6b172b9cf9b7aeaecc1a6f
freerangeandy <freerangeandy@gmail.com>
2020-12-21 18:21:06 -0500

    use component for each experience entry rather than raw HTML

--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -4,6 +4,7 @@ import {
   Grid,
   Segment
 } from 'semantic-ui-react'
+import Experience from './Experience'
 
 const ExperienceSection = (props) => {
   return (
@@ -14,40 +15,40 @@ const ExperienceSection = (props) => {
           <Grid.Column>
             <Container text>
               <h3>Education</h3>
-              <article>
-                <h5>Massachusetts Institute of Technology</h5>
-                <p>Cambridge, MA</p>
-                <p>Bachelor of Science - Mathematics</p>
-                <p>Sept. 2003 - June 2007</p>
-              </article>
-              <article>
-                <h5>Launch Academy</h5>
-                <p>Boston, MA</p>
-                <p>Dec. 2019 - May 2020</p>
-              </article>
+              <Experience
+                name="Massachusetts Institute of Technology"
+                location="Cambridge, MA"
+                degree="Bachelor of Science - Mathematics"
+                date={{ start: "Sept. 2003", end: "June 2007" }}
+              />
+              <Experience
+                name="Launch Academy"
+                location="Boston, MA"
+                date={{ start: "Dec. 2019", end: "May 2020" }}
+              />
             </Container>
           </Grid.Column>
           <Grid.Column>
             <Container text>
               <h3>Work</h3>
-              <article>
-                <h5>Waltham Public Schools</h5>
-                <p>Waltham, MA</p>
-                <p>High School Math Teacher</p>
-                <p>Oct. 2018 - Mar. 2019</p>
-              </article>
-              <article>
-                <h5>Somerville Public Schools</h5>
-                <p>Somerville, MA</p>
-                <p>High School Math Teacher</p>
-                <p>Feb. 2017 - June 2018</p>
-              </article>
-              <article>
-                <h5>Variable Annuity Life Insurance Company (VALIC)</h5>
-                <p>Boston, MA</p>
-                <p>Programmer Analyst</p>
-                <p>Feb. 2008 - Apr. 2015</p>
-              </article>
+              <Experience
+                name="Waltham Public Schools"
+                location="Waltham, MA"
+                title="High School Math Teacher"
+                date={{ start: "Oct. 2018", end: "Mar. 2019" }}
+              />
+              <Experience
+                name="Somerville Public Schools"
+                location="Somerville, MA"
+                title="High School Math Teacher"
+                date={{ start: "Feb. 2017", end: "June 2018" }}
+              />
+              <Experience
+                name="Variable Annuity Life Insurance Company (VALIC)"
+                location="Boston, MA"
+                title="Programmer Analyst"
+                date={{ start: "Feb. 2008", end: "Apr. 2015" }}
+              />                      
             </Container>
           </Grid.Column>
         </Grid.Row>
