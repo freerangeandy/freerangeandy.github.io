26fdf80bf30ec60c1b7bc5e63a99c60823425b33
freerangeandy <freerangeandy@gmail.com>
2020-12-21 15:32:59 -0500

    move personal info up right below name, add github, remove email

--- src/components/AboutMeSection.js
+++ src/components/AboutMeSection.js
@@ -1,11 +1,32 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Container, Icon, List } from 'semantic-ui-react'
 
 const AboutMeSection = (props) => {
   return (
     <Container text>
       <h1>Andy Lee</h1>
-      <p>I'm a recent graduate of the Launch Academy boot camp with 7 years of prior software development experience - and a few years as a high school teacher sandwiched in-between. My favorite coding hobbies are building single-page web apps in JavaScript, trying out new 3rd-party APIs or libraries, and taking online courses in a new language.</p>
+      <List horizontal>
+        <List.Item>
+          <List.Content>
+            <Icon link name='linkedin' />
+            <a href="https://www.linkedin.com/in/andrew-lee-1701d/">LinkedIn</a>
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content>
+            <Icon link name='github' />
+            <a href="https://www.github.com/freerangeandy">GitHub</a>
+          </List.Content>
+        </List.Item>
+      </List>
+
+      <p>
+        I'm a recent graduate of the Launch Academy boot camp with 7 years of
+        prior software development experience - and a few years as a high school
+        teacher sandwiched in-between. My favorite coding hobbies are building
+        single-page web apps in JavaScript, trying out new 3rd-party APIs or
+        libraries, and taking online courses in a new language.
+      </p>
     </Container>
   )
 }
