62fe66b4a75c85e9f07bf36891d28a07685b3b4a
freerangeandy <freerangeandy@gmail.com>
2020-12-21 15:16:29 -0500

    split about-me section into separate component

---
+++ src/components/AboutMeSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+
+const AboutMeSection = (props) => {
+  return (
+    <Container text>
+      <h1>Andy Lee</h1>
+      <p>I'm a recent graduate of the Launch Academy boot camp with 7 years of prior software development experience - and a few years as a high school teacher sandwiched in-between. My favorite coding hobbies are building single-page web apps in JavaScript, trying out new 3rd-party APIs or libraries, and taking online courses in a new language.</p>
+    </Container>
+  )
+}
+export default AboutMeSection
--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -6,6 +6,7 @@ import {
   Menu,
   Button
 } from 'semantic-ui-react'
+import AboutMeSection from './AboutMeSection'
 
 const HeaderSection = (props) => {
   const [fixed, setFixed] = useState(false)
@@ -41,10 +42,7 @@ const HeaderSection = (props) => {
             </Menu.Item>
           </Container>
         </Menu>
-        <Container text>
-          <h1>Andy Lee</h1>
-          <p>I'm a recent graduate of the Launch Academy boot camp with 7 years of prior software development experience - and a few years as a high school teacher sandwiched in-between. My favorite coding hobbies are building single-page web apps in JavaScript, trying out new 3rd-party APIs or libraries, and taking online courses in a new language.</p>
-        </Container>
+        <AboutMeSection />
       </Segment>
     </Visibility>
   )
