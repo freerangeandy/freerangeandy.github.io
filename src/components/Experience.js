516bfe8c5d16ed75e5e71a0683cd93f278c1d224
freerangeandy <freerangeandy@gmail.com>
2020-12-21 18:57:11 -0500

    indented experience info, added location icon

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import { List } from 'semantic-ui-react'
+import { List, Icon } from 'semantic-ui-react'
 
 const Experience = (props) => {
-  const degree = props.degree ? (<List.Item>{props.degree}</List.Item>) : null
-  const title = props.title ? (<List.Item>{props.title}</List.Item>) : null
+  const indent = { style: { marginLeft: '1.4em' }}
+  const degree = props.degree ? (<List.Item {...indent}>{props.degree}</List.Item>) : null
+  const title = props.title ? (<List.Item {...indent}>{props.title}</List.Item>) : null
 
   return (
     <List>
-      <h5>{props.name}</h5>
-      <List.Item>{props.location}</List.Item>
+      <List.Item><h5>{props.name}</h5></List.Item>
+      <List.Item>
+        <Icon name='map marker alternate' /> {props.location}
+      </List.Item>
       {title}
       {degree}
-      <List.Item>{props.date.start} - {props.date.end}</List.Item>
+      <List.Item {...indent}>
+        {props.date.start} &mdash; {props.date.end}
+      </List.Item>
     </List>
   )
 }
