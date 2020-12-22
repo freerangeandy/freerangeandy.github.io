c07bea10887aab8611280c33fb97d953283c351f
freerangeandy <freerangeandy@gmail.com>
2020-12-22 14:47:02 -0500

    added apprentice title to launch academy, moved to top of education list

--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -44,15 +44,16 @@ const ExperienceSection = (props) => {
             >
               <h3>Education</h3>
               <Experience
+                name="Launch Academy"
+                title="Apprentice Full-Stack Developer"
+                location="Boston, MA"
+                date={{ start: "Dec. 2019", end: "May 2020" }}
+              />
+              <Experience
                 name="Massachusetts Institute of Technology"
                 location="Cambridge, MA"
                 degree="Bachelor of Science - Mathematics"
                 date={{ start: "Sept. 2003", end: "June 2007" }}
-              />
-              <Experience
-                name="Launch Academy"
-                location="Boston, MA"
-                date={{ start: "Dec. 2019", end: "May 2020" }}
               />
             </Segment>
           </Grid.Column>
