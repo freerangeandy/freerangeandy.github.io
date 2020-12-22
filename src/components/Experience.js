903430824b646b7bdbe5cb8c5383198cfab33522
freerangeandy <freerangeandy@gmail.com>
2020-12-22 14:44:22 -0500

    reordered experience details, removed location icon

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import { List, Icon } from 'semantic-ui-react'
+import React, { Fragment } from 'react'
+import { List } from 'semantic-ui-react'
 
 const Experience = (props) => {
-  const indent = { style: { marginLeft: '1.4em' }}
-  const degree = props.degree ? (<List.Item {...indent}>{props.degree}</List.Item>) : null
-  const title = props.title ? (<List.Item {...indent}>{props.title}</List.Item>) : null
+  const degree = props.degree ? (<List.Item ><i>{props.degree}</i></List.Item>) : null
+  const title = props.title ? (<List.Item ><i>{props.title}</i></List.Item>) : null
 
   return (
-    <List>
-      <List.Item><h5>{props.name}</h5></List.Item>
-      <List.Item>
-        <Icon name='map marker alternate' /> {props.location}
-      </List.Item>
-      {title}
-      {degree}
-      <List.Item {...indent}>
-        {props.date.start} &mdash; {props.date.end}
-      </List.Item>
-    </List>
+    <Fragment>
+      <h5 style={{ margin:'0.8em 0 0.2em 0' }}>{props.name}</h5>
+      <List style={{ margin:'0 0 0 1.4em' }}>
+        {title}
+        {degree}
+        <List.Item >
+          {props.date.start} &mdash; {props.date.end}
+        </List.Item>
+        <List.Item >{props.location}</List.Item>
+      </List>
+    </Fragment>
   )
 }
 
