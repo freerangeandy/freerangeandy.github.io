7621b134f64b3363258c86ced012be893f9c2083
freerangeandy <freerangeandy@gmail.com>
2020-12-22 15:29:03 -0500

    move live link next to project name, use icon for link

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import {
-  Card
+  Card,
+  Icon
 } from 'semantic-ui-react'
 
 const Project = (props) => {
   return (
     <Card>
       <Card.Header style={{ padding: '0.5em' }}>
-        <h4>{props.name}</h4>
+        <h4>
+          {props.name}&nbsp;&nbsp;&nbsp;
+          <a href={props.siteurl} target="#"><Icon link name='sign-in' /></a>
+        </h4>
       </Card.Header>
       <Card.Content description={props.description} />
       <Card.Content extra>
-      <a href={props.siteurl}>Live</a>&nbsp;
       <a href={props.gitrepo}>GitHub</a>
       </Card.Content>
     </Card>
