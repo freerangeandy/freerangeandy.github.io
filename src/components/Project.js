9d69d4f9306b81a8bc5fc500f46a2274e6408ec3
freerangeandy <freerangeandy@gmail.com>
2020-12-22 14:58:09 -0500

    create separate component for each project entry

---
+++ src/components/Project.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+  Card
+} from 'semantic-ui-react'
+
+const Project = (props) => {
+  return (
+    <article>
+      <h4>{props.title}</h4>
+      <p>{props.description}</p>
+      <a href={props.siteurl}>Live</a>&nbsp;
+      <a href={props.gitrepo}>GitHub</a>
+    </article>
+  )
+}
+
+export default Project
