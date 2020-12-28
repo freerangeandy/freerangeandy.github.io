7fd725373e23c411f803802330bffe22f39cc879
freerangeandy <freerangeandy@gmail.com>
2020-12-28 14:07:17 -0500

    align dates to right for mobile, lighten location in experience items

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,30 +2,35 @@ import React, { Fragment } from 'react'
 import { List } from 'semantic-ui-react'
 
 const Experience = (props) => {
-  const degree = props.degree ? (<List.Item ><i>{props.degree}</i></List.Item>) : null
-  const title = props.title ? (<List.Item ><i>{props.title}</i></List.Item>) : null
+  const degree = props.degree ? (<i>{props.degree}</i>) : null
+  const title = props.title ? (<i>{props.title}</i>) : null
 
   const experienceEntry = props.mobile
   ? (<Fragment>
       <h5 style={{ margin:'0.8em 0 0.2em 0' }}>{props.name}</h5>
-      <List horizontal>
-        {title}
-        {degree}
+      <List
+        horizontal
+        style={{ display: "flex", justifyContent:"space-between" }}
+      >
+        <List.Item>
+          {title}{degree}
+          <List.Description style={{marginTop: "0.3em"}}>{props.location}</List.Description>
+        </List.Item>
         <List.Item >
           {props.date.start} &mdash; {props.date.end}
         </List.Item>
       </List>
-      <List.Description >{props.location}</List.Description>
     </Fragment>)
   : (<Fragment>
       <h5 style={{ margin:'0.8em 0 0.2em 0' }}>{props.name}</h5>
       <List style={{ margin:'0 0 0 1.4em' }}>
-        {title}
-        {degree}
-        <List.Item >
-          {props.date.start} &mdash; {props.date.end}
+        <List.Item>
+          {title}{degree}
         </List.Item>
-        <List.Item >{props.location}</List.Item>
+        <List.Item>
+          {props.date.start} &mdash; {props.date.end}
+          <List.Description style={{marginTop: "0.3em"}}>{props.location}</List.Description>
+        </List.Item>
       </List>
     </Fragment>)
 
