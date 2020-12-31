7c40ea427d4610b121fc89f7b66054e6b34a9d90
freerangeandy <freerangeandy@gmail.com>
2020-12-31 17:02:36 -0500

    display resume PDF in portal window

--- src/components/PDFViewPortal.js
+++ src/components/PDFViewPortal.js
@@ -1,32 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Button, Segment, Portal } from 'semantic-ui-react'
+import { Document, Page } from 'react-pdf/dist/esm/entry.parcel'
+import PdfFile from '../../assets/cv.pdf'
 
 const PDFViewPortal = (props) => {
-  const [open, setOpen] = useState(false)
-
   return (
     <Portal
       openOnTriggerClick
       closeOnTriggerClick
-      trigger={
-        <Button
-          inverted={!props.fixed}
-        >
-          Resumé
-        </Button>
-      }
-      onOpen={()=>setOpen(true)}
-      onClose={()=>setOpen(false)}
+      trigger={ <Button inverted={!props.fixed}> Resumé </Button> }
+      onOpen={()=>console.log("open")}
+      onClose={()=>console.log("close")}
     >
-      <Segment
-        style={{
-          left: '5%',
-          top: '5%',
-          position: 'fixed',
-          zIndex: 1000,
-        }}
-      >
-      <h2>h22222</h2>
+      <Segment style={{ top:'6%', right:'3%', position:'fixed', zIndex:1000 }}>
+        <Document file={PdfFile}>
+          <Page pageNumber={1} renderTextLayer={false} />
+        </Document>
       </Segment>
     </Portal>
   )
